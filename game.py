338ecc2a7ce8f6656c7b41406af86cff1f68e2dc
NarShaddaa <radius-unos@mail.ru>
2025-03-11 15:25:54 +0300

    sprint 3 x0 game

---
+++ game.py
@@ -0,0 +1,19 @@
+# Объявить класс.
+from gameparts import Board
+
+
+# Создать игровое поле - объект класса Board.
+def main():
+    game = Board()
+    print(type(game))
+# Отрисовать поле в терминале.
+    game.display()
+# Разместить на поле символ по указанным координатам - сделать ход.
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+# Перерисовать поле с учётом сделанного хода.
+    game.display()
+
+
+if __name__ == '__main__':
+    main()
