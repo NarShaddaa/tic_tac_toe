8b6a44850a68e58d3e4ecbaf87ecaf2651af3820
NarShaddaa <radius-unos@mail.ru>
2025-03-13 12:37:51 +0300

    updatedXO

--- game.py
+++ game.py
@@ -4,6 +4,12 @@ from gameparts import Board
 from gameparts.exceptions import CellOccupiedError, FieldIndexError
 
 
+def save_result(result):
+    file = open('results.txt', 'a', encoding='utf-8')
+    file.write(result + '\n')
+    file.close()
+
+
 def main():
     game = Board()
     current_player = 'X'
@@ -48,10 +54,14 @@ def main():
         game.display()
         # После каждого хода надо делать проверку на победу и на ничью.
         if game.check_win(current_player):
-            print(f'Победили {current_player}.')
+            result = (f'Победили {current_player}.')
+            print(result)
+            save_result(result)
             running = False
         elif game.is_board_full():
-            print('Ничья!')
+            result = print('Ничья!')
+            print(result)
+            save_result(result)
             running = False
 
         current_player = 'O' if current_player == 'X' else 'X'
